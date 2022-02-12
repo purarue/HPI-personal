2609f278d5bcd9485df468e9bf7ee842e2fb6a78
Sean Breckenridge <seanbrecke@gmail.com>
2022-02-11 18:17:28 -0800

    add recent history scripts

---
+++ scripts/recent_history.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Copy, open and print url/metadata from
+the current firefox database
+
+Used with the recent_history script
+"""
+
+import json
+from browserexport.browsers.firefox import Firefox
+from browserexport.parse import read_visits
+from sqlite_backup import sqlite_backup
+from more_itertools import unique_everseen
+
+conn = sqlite_backup(Firefox.locate_database())
+assert conn is not None
+for visit in unique_everseen(read_visits(conn), key=lambda v: v.url):
+    print(
+        json.dumps(
+            {"url": visit.url, "metadata": getattr(visit.metadata, "description", "")}
+        )
+    )
