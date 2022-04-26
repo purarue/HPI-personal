6b8fcdff134871c87e15cf1600abc17e5d0a8ff2
Sean Breckenridge <seanbrecke@gmail.com>
2022-04-25 18:14:46 -0700

    add personal all.py file

---
+++ my/location/all.py
@@ -0,0 +1,32 @@
+"""
+Merges location data from multiple sources -- this includes apple location data as well
+"""
+
+from typing import Iterator
+
+from my.core import Stats, LazyLogger
+
+from my.location.via_ip import locations
+
+from my.location import via_ip, google_takeout, gpslogger
+from my.location import apple  # additional source
+
+from my.location.common import Location
+
+
+logger = LazyLogger(__name__, level="warning")
+
+
+def locations() -> Iterator[Location]:
+    yield from google_takeout.locations()
+    yield from via_ip.locations()
+    yield from gpslogger.locations()
+    yield from apple.locations()
+
+
+def stats() -> Stats:
+    from my.core import stat
+
+    return {
+        **stat(locations),
+    }
