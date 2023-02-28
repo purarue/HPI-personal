dcf7a101947c268df61cd3cc697209f6660df283
Sean Breckenridge <seanbrecke@gmail.com>
2023-02-27 20:44:20 -0800

    all: remove via_ip from locations

--- my/location/all.py
+++ my/location/all.py
@@ -6,7 +6,7 @@ from typing import Iterator
 
 from my.core import Stats, LazyLogger
 
-from my.location import via_ip, google_takeout, gpslogger
+from my.location import google_takeout, gpslogger
 from my.location import apple  # additional source
 
 from my.location.common import Location
@@ -17,7 +17,6 @@ logger = LazyLogger(__name__, level="warning")
 
 def locations() -> Iterator[Location]:
     yield from google_takeout.locations()
-    yield from via_ip.locations()
     yield from gpslogger.locations()
     yield from apple.locations()
 
