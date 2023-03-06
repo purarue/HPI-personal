3f4a078fcb3cd6215bd3154a673801ced4242b3d
Sean Breckenridge <seanbrecke@gmail.com>
2023-03-05 17:47:39 -0800

    location/all: include semantic location data

--- my/location/all.py
+++ my/location/all.py
@@ -6,8 +6,9 @@ from typing import Iterator
 
 from my.core import Stats, LazyLogger
 
-from my.location import google_takeout, gpslogger
 from my.location import apple  # additional source
+from my.location import google_takeout, gpslogger, google_takeout_semantic
+from my.util.iter import warn_exceptions
 
 from my.location.common import Location
 
@@ -16,6 +17,7 @@ logger = LazyLogger(__name__, level="warning")
 
 
 def locations() -> Iterator[Location]:
+    yield from warn_exceptions(google_takeout_semantic.locations())
     yield from google_takeout.locations()
     yield from gpslogger.locations()
     yield from apple.locations()
